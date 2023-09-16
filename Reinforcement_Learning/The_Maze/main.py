82141069c2e9a03ddda5b2ba4c270b53f78ada2c
e-hokage777 <e.hokage777@gmail.com>
2023-09-16 13:08:10 +0000

    added agent and enemy images

--- Reinforcement_Learning/The_Maze/main.py
+++ Reinforcement_Learning/The_Maze/main.py
@@ -19,7 +19,7 @@ class Game(Widget):
     NUM_TILES_H = NumericProperty(10)
     NUM_TILES_V = NumericProperty(10)
     NUM_TILES = ReferenceListProperty(NUM_TILES_H, NUM_TILES_V)
-    NUM_PITS = 5
+    NUM_ENEMIES = 5
     maze = ObjectProperty(None)
 
     def __init__(self, **kwargs):
@@ -46,7 +46,7 @@ class Maze(RelativeLayout):
     tile_height = 0
     tiles = []
     goal_idx = None
-    pits_idxs = []
+    enemies_idxs = []
     tile_reward_array = None
 
     def __init__(self, **kwargs):
@@ -56,29 +56,40 @@ class Maze(RelativeLayout):
     def init_tiles(self):
         self.tile_width = self.width / self.num_tiles[0]
         self.tile_height = self.height / self.num_tiles[1]
-        self.goal_idx = (self.num_tiles[0]-1, self.num_tiles[1]-1)
+        self.goal_idx = (self.num_tiles[0] - 1, self.num_tiles[1] - 1)
 
         ## setting up pit locations
-        pits_xs = random.sample([r for r in range(self.num_tiles[0])], self.num_pits)
-        pits_ys = random.sample([r for r in range(self.num_tiles[1])], self.num_pits)
-        self.pits_idxs = list(zip(pits_xs, pits_ys))
+        enemies_xs = random.sample([r for r in range(self.num_tiles[0])], self.num_enemies)
+        enemies_ys = random.sample([r for r in range(self.num_tiles[1])], self.num_enemies)
+        self.enemies_idxs = list(zip(enemies_xs, enemies_ys))
 
         with self.canvas.before:
+            tile_img = "assets/tile.png"
+            enemy_img = "assets/enemy.png"
             for i in range(self.num_tiles[0]):
                 for j in range(self.num_tiles[1]):
                     xpos = i * self.tile_width
                     ypos = j * self.tile_height
-                    if (i, j) ==  self.goal_idx:
+                    if (i, j) == self.goal_idx:
                         Color(rgb=(0, 1, 0))
-                    elif (i, j) in self.pits_idxs:
+                    elif (i, j) in self.enemies_idxs:
                         Color(rgb=(1, 0, 0))
-                    else:
-                        Color(rgb=(0, 0, 0))
+                    # else:
+                    #     Color(rgba=(1, 1, 1))
                     self.tiles.append(
                         Rectangle(
-                            size=(self.tile_width, self.tile_height), pos=(xpos, ypos)
+                            size=(self.tile_width, self.tile_height), pos=(xpos, ypos),
+                            source=tile_img
                         )
                     )
+
+                    if (i, j) in self.enemies_idxs:
+                        Color("transparent")
+                        Rectangle(
+                            size=(self.tile_width, self.tile_height), pos=(xpos, ypos),
+                            source = enemy_img
+                        )
+
                     Color(rgb=(1, 1, 1))
                     Line(rectangle=(xpos, ypos, self.tile_width, self.tile_height))
 
@@ -86,7 +97,7 @@ class Maze(RelativeLayout):
         self.tile_reward_array = np.zeros((self.num_tiles[0], self.num_tiles[1]))
         self.tile_reward_array[self.goal_idx] = 1
 
-        for coords in self.pits_idxs:
+        for coords in self.enemies_idxs:
             self.tile_reward_array[coords] = -1
 
     def init_agent(self):
@@ -99,10 +110,20 @@ class Maze(RelativeLayout):
         self.init_tile_reward_array()
         self.init_agent()
 
-    ## functiont o move agent
+    def reset_game(self):
+        self.agent.reset()
+
+    ## functiont to move agent
     def move_agent(self, keycode):
         if not self.is_at_edge(self.agent, keycode[1]):
             self.agent.move(keycode, (self.tile_width, self.tile_height))
+
+        ## resetting player position when goal reached
+        if (
+            self.agent.pos_idxs[0] == self.goal_idx[0]
+            and self.agent.pos_idxs[1] == self.goal_idx[1]
+        ):
+            self.reset_game()
 
     ## function to check whether object is at edge of maze
     def is_at_edge(self, obj, direction):
@@ -116,7 +137,7 @@ class Maze(RelativeLayout):
             return True
 
         return False
-    
+
         ## some old logic that might be useful later
         # if(obj.pos[0] >= (self.width - self.tile_width)) and direction == "right":
         #     return True
@@ -129,6 +150,11 @@ class Maze(RelativeLayout):
 
 
 class Agent(Widget):
+    ## function to reset player to beginning of world
+    def reset(self):
+        self.pos = (0, 0)
+        self.pos_idxs = [0,0]
+
     def move(self, keycode, dist):
         # print(self.pos_idxs)
         # if(keycode[1] == "right"):
