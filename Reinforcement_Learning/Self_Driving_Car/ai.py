4df1b0ed17d041ac116455e362ac13a2db146c6b
e-hokage777 <e.hokage777@gmail.com>
2023-10-05 13:25:02 +0000

    added function to select an action

--- Reinforcement_Learning/Self_Driving_Car/ai.py
+++ Reinforcement_Learning/Self_Driving_Car/ai.py
@@ -27,12 +27,14 @@ class ReplayMemory:
         self.capacity = capacity
         self.memory = []
 
+    ## function to push into the memory
     def push(self, event):
         self.memory.append(event)
 
         if event.length > self.capacity:
             del self.memory[0]
 
+    ## function to select `batch_size` samples
     def sample(self, batch_size):
         samples = zip(*random.sample(self.memory, batch_size))
         return map(lambda x: torch.tensor(x, requires_grad=True).reshape(-1,1), samples) ## check here well
@@ -49,3 +51,9 @@ class Dqn:
         self.last_state = torch.randn(input_size).unsqueeze(0)
         self.last_action = 0
         self.last_reward = 0
+
+    ## function to select an action
+    def select_action(self, state):
+        probs = F.softmax(self.model(state)*7)
+        action = probs.multinomial(1)
+        return action.item()
